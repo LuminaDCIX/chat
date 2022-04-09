8aac25aeb081fab44858069d5da981ed076e45bf
lumina <aster2632@gmail.com>
2022-04-09 16:49:42 +0800

    fix prefix of ip

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 import os
 from app import create_app, socketio
 from app.chain import *
-
+import time
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, use_reloader=False, host='0.0.0.0', port=8889,)
